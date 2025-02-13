1302572148947c6c07c8160f586ce1c13a32ecc7
sreejithinfysec <62929735+sreejithinfysec@users.noreply.github.com>
2025-02-13 16:33:19 +0530

    Fixing src/main/java/com/datadoghq/workshops/samplevulnerablejavaapp/controller/MainController.java for finding 2

--- src/main/java/com/datadoghq/workshops/samplevulnerablejavaapp/controller/MainController.java
+++ src/main/java/com/datadoghq/workshops/samplevulnerablejavaapp/controller/MainController.java
@@ -53,17 +53,19 @@ public class MainController {
     return new ResponseEntity<>(result, HttpStatus.OK);
   }
 
-  @RequestMapping(method=RequestMethod.POST, value="/view-file", consumes="application/json")
-  public ResponseEntity<String> viewFile(@RequestBody ViewFileRequest request) {
+@RequestMapping(method=RequestMethod.POST, value="/view-file", consumes="application/json")
+public ResponseEntity<String> viewFile(@RequestBody ViewFileRequest request) {
     log.info("Reading file " + request.path);
     try {
-      String result = fileService.readFile(request.path);
-      return new ResponseEntity<>(result, HttpStatus.OK);
+        String result = fileService.readFile(request.path);
+        return new ResponseEntity<>(result, HttpStatus.OK);
     } catch (FileForbiddenFileException e) {
-      return new ResponseEntity<>(e.getMessage(), HttpStatus.FORBIDDEN);
+        return new ResponseEntity<>(e.getMessage(), HttpStatus.FORBIDDEN);
     } catch (FileReadException e) {
-      return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+        return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
     }
+}
+
   }
 
 }
