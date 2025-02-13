4790e998efbedec332df5ebaa85b50ffb8852fa8
sreejithinfysec <62929735+sreejithinfysec@users.noreply.github.com>
2025-02-13 16:33:18 +0530

    Fixing src/main/java/com/datadoghq/workshops/samplevulnerablejavaapp/service/FileService.java for finding 2

--- src/main/java/com/datadoghq/workshops/samplevulnerablejavaapp/service/FileService.java
+++ src/main/java/com/datadoghq/workshops/samplevulnerablejavaapp/service/FileService.java
@@ -10,22 +10,28 @@ import java.io.*;
 public class FileService {
     final static String ALLOWED_PREFIX = "/tmp/files/";
 
-    public String readFile(String path) throws FileForbiddenFileException, FileReadException {
-        if(!path.startsWith(ALLOWED_PREFIX)) {
-            throw new FileForbiddenFileException("You are not allowed to read " + path);
-        }
-        try (BufferedReader br = new BufferedReader(new FileReader(path))) {
-            StringBuilder sb = new StringBuilder();
-            String line = br.readLine();
+public String readFile(String path) throws FileForbiddenFileException, FileReadException {
+    if(!isValidPath(path)) {
+        throw new FileForbiddenFileException("You are not allowed to read " + path);
+    }
+    try (BufferedReader br = new BufferedReader(new FileReader(path))) {
+        StringBuilder sb = new StringBuilder();
+        String line = br.readLine();
 
-            while (line != null) {
-                sb.append(line);
-                sb.append(System.lineSeparator());
-                line = br.readLine();
-            }
-            return sb.toString();
-        } catch (IOException e) {
-            throw new FileReadException(e.getMessage());
+        while (line != null) {
+            sb.append(line);
+            sb.append(System.lineSeparator());
+            line = br.readLine();
         }
+        return sb.toString();
+    } catch (IOException e) {
+        throw new FileReadException(e.getMessage());
+    }
+}
+
+private boolean isValidPath(String path) {
+    return path.startsWith(ALLOWED_PREFIX) && !Pattern.matches(".*[\\.\\/]\\.\\/.*", path);
+}
+
     }
 }
